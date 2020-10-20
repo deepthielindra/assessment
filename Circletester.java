1baa3deec4e5193e141c295b3afcfe5bd8a9d449
deepthielindra <73148267+deepthielindra@users.noreply.github.com>
2020-10-20 23:11:30 +0530

    Add files via upload

---
+++ Circletester.java
@@ -0,0 +1,11 @@
+package com.daay2;
+
+public class Circletester {
+
+	public static void main(String[] args) {
+		Circle c2 = new Circle(2.0);
+
+		System.out.println("The circle has radius of " + c2.getRadius() + " and area of " + c2.getArea());
+	}
+
+}
---
+++ CurrentAcount.java
@@ -0,0 +1,30 @@
+package q3;
+
+
+	public class CurrentAcount extends Account {
+		
+		private int tradeLicenseNumber;
+		private static final double MIN_BALANCE = 500.0;
+		
+		public CurrentAcount() {
+			
+		}
+		
+		public CurrentAcount(String memberName, double accountBalance) {
+			super(memberName, accountBalance);
+		}
+		
+		public void getBalance() {
+			System.out.println(getAccountBalance());
+		}
+
+		public void withDraw(double amount) {
+			if((getAccountBalance() - amount) >= MIN_BALANCE ) {
+				setAccountBalance(getAccountBalance() - amount);
+			} else {
+				System.out.println("Insufficient funds");
+			}
+		}
+	}
+
+
